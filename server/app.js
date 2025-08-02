d95796713c59829357bc27fb9f4d5f04678b5489
Ton Tran <trancanhton657@gmail.com>
2025-08-02 22:44:10 +0300

    fix: fix for render

--- server/app.js
+++ server/app.js
@@ -43,8 +43,8 @@ const xss = require("xss-clean");
 const mongoSanitize = require("express-mongo-sanitize");
 
 // routes
-// app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/users", userRouter);
 app.use("/api/v1/bikes", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/orders", orderRouter);
